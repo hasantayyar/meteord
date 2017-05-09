4138f4bb56bdb562bfd991cddd7fb22319f1fbab
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2017-05-09 15:31:56 +0200

    added libcairo2-dev libjpeg-dev libgif-dev packages

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y curl bzip2 build-essential g++ python git
+apt-get install -y libcairo2-dev libjpeg-dev libgif-dev curl bzip2 build-essential g++ python git
